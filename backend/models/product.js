7b3c3d9ac9682b81b9d31af36d7cb090c448efa0
ntritin62 <21522677@gm.uit.edu.vn>
2023-08-20 01:22:44 +0700

    Add Product Model

---
+++ backend/models/product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  oldprice: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Product', productSchema);
