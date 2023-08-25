bd3af3f9785f25f43c5822288efdb6a7bd13574f
ntritin62 <21522677@gm.uit.edu.vn>
2023-08-25 16:01:41 +0700

    fix bug

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,11 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK',
+    });
+  }
   next();
 });
 
